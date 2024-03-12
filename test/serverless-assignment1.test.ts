838f8cf85d783556300638a2f5d5ac38c2d128a5
Jingyi Wang <20104728@email.wit.ie>
2024-03-12 00:12:24 +0000

    Initial commit

---
+++ test/serverless-assignment1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessAssignment1 from '../lib/serverless-assignment1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-assignment1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessAssignment1.ServerlessAssignment1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
