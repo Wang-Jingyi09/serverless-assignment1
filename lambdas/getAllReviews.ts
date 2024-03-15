ef54f800057943f2ca6cb116a1e655f959937e69
Jingyi Wang <20104728@email.wit.ie>
2024-03-15 23:54:59 +0000

    Lambda A: Expand it to allow for query string parameters

--- lambdas/getAllReviews.ts
+++ lambdas/getAllReviews.ts
@@ -1,36 +1,57 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, content) => {
     const movieId = event.pathParameters?.movieId;
+    const minRating = event.queryStringParameters?.minRating;
+
     if (!movieId) {
         return {
             statusCode: 400,
             body: JSON.stringify({ message: "Missing movieId" }),
+            headers: { 'Content-Type': 'application/json' },
+        };
+    }
+
+    // let queryParams = {
+    //     TableName: process.env.TABLE_NAME,
+    //     KeyConditionExpression: 'MovieId = :movieId',
+    //     ExpressionAttributeValues: {
+    //       ':movieId': { N: String(movieId) },
+    //     },
+    //   };
+    let queryParams: QueryCommandInput = {
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: 'MovieId = :movieId',
+        ExpressionAttributeValues: {
+            ':movieId': parseInt(movieId),
+        } as Record<string, any>, 
+    };
+    if (minRating) {
+        queryParams = {
+            ...queryParams,
+            FilterExpression : 'Rating >= :minRating',
+            ExpressionAttributeValues: { 
+                ...queryParams.ExpressionAttributeValues, 
+                ':minRating': Number(minRating) }
         };
     }
 
     try {
         // Print Event
         console.log("Event: ", event);
-        const commandOutput = await ddbDocClient.send(
-            new QueryCommand({
-                TableName: process.env.TABLE_NAME,
-                KeyConditionExpression: "MovieId = :movieId",
-                ExpressionAttributeValues: {
-                    ":movieId": parseInt(movieId), 
-                },
-            })
-        );
+
+        const commandOutput = await ddbDocClient.send(new QueryCommand(queryParams));
 
         // check if there is a review
         if (!commandOutput.Items || commandOutput.Items.length === 0) {
             return {
                 statusCode: 404,
                 body: JSON.stringify({ message: "No reviews found for this movie" }),
+                headers: { 'Content-Type': 'application/json' },
             };
         }
         return {
@@ -43,6 +64,7 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, content) => {
         return {
             statusCode: 500,
             body: JSON.stringify({ message: "Could not fetch movie reviews", error }),
+            headers: { 'Content-Type': 'application/json' },
         };
     }
 };
